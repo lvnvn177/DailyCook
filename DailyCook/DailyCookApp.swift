443a75f09b240bcc4374ddf32ba7ab952f2768dc
lvnvn177 <53424393+lvnvn177@users.noreply.github.com>
2024-11-23 13:43:59 +0900

    Initial Commit

---
+++ DailyCook/DailyCookApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyCookApp.swift
+//  DailyCook
+//
+//  Created by 이영호 on 11/23/24.
+//
+
+import SwiftUI
+
+@main
+struct DailyCookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
